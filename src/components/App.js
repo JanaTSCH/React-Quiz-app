fbd5bc81bd1984843b5dab4d638b09183e223038
Jana-script <jana.jscript@gmail.com>
2025-01-31 22:21:43 +0100

    displaying questions

---
+++ src/components/App.js
@@ -0,0 +1,68 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+// import DateCounter from "./DateCounter";
+// import "./App.css";
+
+const initialState = {
+  questions: [],
+
+  //'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+  index: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
+
+    // case "loading":
+    //   return { ...state, status: "loading" };
+    // case "ready":
+    //   return { ...state, status: "ready" };
+
+    // case "finished":
+    //   return { ...state, status: "finished" };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+export default function App() {
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
+      </Main>
+    </div>
+  );
+}
